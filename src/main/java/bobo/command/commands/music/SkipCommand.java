970bb222cd8a0b451935cee010ffe12c9045e373
Gil Anavi <gil.j.anavi@gmail.com>
2023-03-01 14:51:01 +0200

    fixed skip with looping and edited queue

--- src/main/java/bobo/command/commands/music/SkipCommand.java
+++ src/main/java/bobo/command/commands/music/SkipCommand.java
@@ -3,6 +3,7 @@ package bobo.command.commands.music;
 import bobo.command.ICommand;
 import bobo.lavaplayer.GuildMusicManager;
 import bobo.lavaplayer.PlayerManager;
+import bobo.lavaplayer.TrackScheduler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
@@ -11,11 +12,13 @@ public class SkipCommand implements ICommand {
     public void handle(SlashCommandInteractionEvent event) {
         final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(event.getGuildChannel().getGuild());
         final AudioPlayer audioPlayer = musicManager.audioPlayer;
+        final TrackScheduler scheduler = musicManager.scheduler;
         if (audioPlayer.getPlayingTrack() == null) {
             event.reply("There is nothing currently playing").queue();
         } else {
-            event.reply("Skipped").queue();
-            musicManager.scheduler.nextTrack();
+            event.reply(scheduler.looping ? "Skipped. Looping has been turned off" : "Skipped").queue();
+            scheduler.looping = false;
+            scheduler.nextTrack();
         }
     }
 
