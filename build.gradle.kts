ee23056aaefb77be7436c93406fb19cd3fed1fc2
Gil Anavi <gil.j.anavi@gmail.com>
2023-04-20 18:22:25 +0300

    Add javax.annotation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("net.dv8tion:JDA:5.0.0-beta.8")
