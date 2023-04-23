894ce5f6ceacc3a330cf31b4fd39757dbf627a44
Gil Anavi <gil.j.anavi@gmail.com>
2023-04-24 01:12:44 +0300

    Uncommented isYouTubeURL

--- src/main/java/bobo/utils/YouTubeUtil.java
+++ src/main/java/bobo/utils/YouTubeUtil.java
@@ -48,16 +48,16 @@ public class YouTubeUtil {
 
         return "https://www.youtube.com/watch?v=" + videoId;
     }
-/*
+
     /**
      * @param youTubeURL the string to check if it's a link
      * @return true if given string is a YouTube link, false otherwise
-     *
+     */
     public static boolean isYouTubeURL(String youTubeURL) {
         String pattern = "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(\\.com)?/.+";
         return !youTubeURL.isEmpty() && youTubeURL.matches(pattern);
     }
-*/
+
     /**
      * @param youTubeURL the YouTube link
      * @return the id of the given YouTube link
