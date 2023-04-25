676b255420759d650c4081e2376d0f1d0cf1c63c
Gil Anavi <gil.j.anavi@gmail.com>
2023-04-25 16:38:35 +0300

    Implemented pagination to search command

--- src/main/java/bobo/command/commands/SearchCommand.java
+++ src/main/java/bobo/command/commands/SearchCommand.java
@@ -2,6 +2,9 @@ package bobo.command.commands;
 
 import bobo.Config;
 import bobo.command.ICommand;
+import com.github.ygimenez.method.Pages;
+import com.github.ygimenez.model.InteractPage;
+import com.github.ygimenez.model.Page;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -9,8 +12,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import javax.annotation.Nonnull;
 import java.awt.*;
@@ -20,20 +21,20 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SearchCommand implements ICommand {
     private static final String API_KEY = Config.get("GOOGLE_API_KEY");
     private static final String SEARCH_ENGINE_ID = Config.get("SEARCH_ENGINE_ID");
 
-    private JsonArray items;
-    private int totalPages;
-    private MessageEmbed currentEmbed;
-
     @Override
     public void handle(@Nonnull SlashCommandInteractionEvent event) {
-        String query = Objects.requireNonNull(event.getOption("query")).getAsString();
+        int numPages;
+        JsonArray images;
         try {
+            String query = Objects.requireNonNull(event.getOption("query")).getAsString();
             String url = "https://www.googleapis.com/customsearch/v1?key=" + API_KEY +
                     "&cx=" + SEARCH_ENGINE_ID + "&q=" + URLEncoder.encode(query, StandardCharsets.UTF_8) +
                     "&searchType=image";
@@ -43,56 +44,46 @@ public class SearchCommand implements ICommand {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
 
-            items = json.getAsJsonArray("items");
-            totalPages = items.size();
-            if (totalPages == 0) {
+            images = json.getAsJsonArray("items");
+            numPages = images.size();
+            if (numPages == 0) {
                 event.reply("No images found for query: " + query).queue();
                 return;
             }
 
-            sendPage(event, 1);
-
         } catch (Exception e) {
             event.reply("Error: " + e.getMessage()).queue();
+            return;
         }
-    }
 
-    private void sendPage(@Nonnull SlashCommandInteractionEvent event, int page) {
+        final List<Page> pages = new ArrayList<>();
+        MessageEmbed embed;
         Member member = event.getMember();
-        JsonObject item = items.get(page - 1).getAsJsonObject();
-        String title = item.get("title").getAsString();
-        String imageUrl = item.get("link").getAsString();
-        String imageContextUrl = item.getAsJsonObject().get("image").getAsJsonObject().get("contextLink").getAsString();
+        for (int i = 0; i < numPages; i++) {
+            JsonObject image = images.get(i).getAsJsonObject();
+            String title = image.get("title").getAsString();
+            String imageUrl = image.get("link").getAsString();
+            String imageContextUrl = image.getAsJsonObject().get("image").getAsJsonObject().get("contextLink").getAsString();
 
-        assert member != null;
-        MessageEmbed embed = new EmbedBuilder()
-                .setAuthor(member.getUser().getAsTag(), "https://discord.com/users/" + member.getId(), member.getAvatarUrl())
-                .setTitle("Search Results")
-                .setFooter("Page " + page + "/" + totalPages)
-                .setColor(Color.red)
-                .addField(title, imageContextUrl, true)
-                .setImage(imageUrl)
-                .build();
-
-        Button prevButton = Button.secondary("prev", "Prev").withDisabled(page == 1);
-        Button nextButton = Button.secondary("next", "Next").withDisabled(page == totalPages);
-
-        if (currentEmbed == null) {
-            event.replyEmbeds(embed).addActionRow(prevButton, nextButton).queue();
-        } else {
-            event.getHook().editOriginalEmbeds(embed).queue();
+            assert member != null;
+            embed = new EmbedBuilder()
+                    .setAuthor(member.getUser().getAsTag(), "https://discord.com/users/" + member.getId(), member.getAvatarUrl())
+                    .setTitle("Search Results")
+                    .setFooter("Page " + (i + 1) + "/" + numPages)
+                    .setColor(Color.red)
+                    .addField(title, imageContextUrl, true)
+                    .setImage(imageUrl)
+                    .build();
+            pages.add(i, new InteractPage(embed));
         }
-        currentEmbed = embed;
-    }
 
-    public void handleButtonClick(@Nonnull ButtonInteractionEvent event, int currentPage) {
-        String buttonId = event.getComponentId();
+        if (pages.size() == 1) {
+            event.replyEmbeds((MessageEmbed) pages.get(0).getContent()).queue();
+        } else {
+            event.getMessageChannel().sendMessageEmbeds((MessageEmbed) pages.get(0).getContent()).queue(success -> Pages.paginate(success, pages, true));
+            event.reply("Search result sent").setEphemeral(true).queue();
+        }
 
-        if (buttonId.equals("prev")) currentPage--;
-        else if (buttonId.equals("next")) currentPage++;
-
-        //sendPage(currentPage);
-        event.deferEdit().queue();
     }
 
     @Override
