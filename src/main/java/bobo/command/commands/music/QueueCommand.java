06210cdfe8026229273baedf83edf75574fb74c0
Gil Anavi <gil.j.anavi@gmail.com>
2023-04-25 16:39:06 +0300

    Changed queue to send embedded message instead of text

--- src/main/java/bobo/command/commands/music/QueueCommand.java
+++ src/main/java/bobo/command/commands/music/QueueCommand.java
@@ -10,8 +10,10 @@ import com.github.ygimenez.model.InteractPage;
 import com.github.ygimenez.model.Page;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -38,39 +40,38 @@ public class QueueCommand implements ICommand {
         AudioTrack track;
         AudioTrackInfo info;
         int count = 0;
-        MessageCreateBuilder message = new MessageCreateBuilder()
-                .addContent("**Current Queue:** Page 1/" + (int) Math.ceil((double) trackList.size() / 10) + "\n");
-        for (int i = 0; i < trackList.size(); i++) {
+        int numPages = trackList.size();
+        Member member = event.getMember();
+        assert member != null;
+        EmbedBuilder builder = new EmbedBuilder()
+                .setAuthor(member.getUser().getAsTag(), "https://discord.com/users/" + member.getId(), member.getAvatarUrl())
+                .setTitle("Current Queue")
+                .setFooter("Page 1/" + (int) Math.ceil((double) numPages / 10));
+
+        for (int i = 0; i < numPages; i++) {
             track = trackList.get(i);
             info = track.getInfo();
-            message.addContent(String.valueOf(i + 1))
-                    .addContent(": ")
-                    .addContent(info.title)
-                    .addContent(" by **")
-                    .addContent(info.author)
-                    .addContent("** [")
-                    .addContent(i == 0 ? TimeFormat.formatTime(track.getDuration() - track.getPosition()) : TimeFormat.formatTime(track.getDuration()))
-                    .addContent(i == 0 ? (scheduler.looping ? " left] (currently looping)\n" : " left] (currently playing)\n") : "]\n");
+            builder.addField((i + 1) + ":", "[" + info.title + "](" + info.uri + ") by **" + info.author + "** [" + (i == 0 ? TimeFormat.formatTime(track.getDuration() - track.getPosition()) : TimeFormat.formatTime(track.getDuration())) + (i == 0 ? (scheduler.looping ? " left] (currently looping)\n" : " left] (currently playing)\n") : "]\n"), false);
             count++;
             if (count == 10) {
-                pages.add(new InteractPage(message.getContent()));
-                message = new MessageCreateBuilder()
-                        .addContent("**Current Queue:** Page " + ((int) Math.ceil((double) (i + 1) / 10) + 1) + "/" + (int) Math.ceil((double) trackList.size() / 10) + "\n");
+                pages.add(new InteractPage(builder.build()));
+                builder = new EmbedBuilder()
+                        .setAuthor(member.getUser().getAsTag(), "https://discord.com/users/" + member.getId(), member.getAvatarUrl())
+                        .setTitle("Current Queue")
+                        .setFooter("Page " + ((int) Math.ceil((double) (i + 1) / 10) + 1) + "/" + (int) Math.ceil((double) numPages / 10));
                 count = 0;
             }
         }
-        if (!message.isEmpty()) {
-            pages.add(new InteractPage(message.getContent()));
+        if (!builder.getFields().isEmpty()) {
+            pages.add(new InteractPage(builder.build()));
         }
 
         if (pages.size() == 1) {
-            event.reply((String) pages.get(0).getContent()).queue();
+            event.replyEmbeds((MessageEmbed) pages.get(0).getContent()).queue();
         } else {
-            event.getMessageChannel().sendMessage((String) pages.get(0).getContent()).queue(success -> Pages.paginate(success, pages, true));
-            event.reply("Queue sent").queue();
+            event.getMessageChannel().sendMessageEmbeds((MessageEmbed) pages.get(0).getContent()).queue(success -> Pages.paginate(success, pages, true));
+            event.reply("Queue sent").setEphemeral(true).queue();
         }
-
-
     }
 
     @Override
