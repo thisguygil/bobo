a0b218e6d342886271b8f118812739db4064baee
Gil Anavi <gil.j.anavi@gmail.com>
2023-05-02 20:47:05 +0300

    Gave Bobo a purpose in life

--- src/main/python/bobo/boboAI.py
+++ src/main/python/bobo/boboAI.py
@@ -6,17 +6,26 @@ from flask import Flask, request
 boboAI = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-messages = []
+
+def initialize_messages():
+    return [{"role": "system",
+             "content": """You are Bobo, a fun and helpful Discord bot created by Gil Anavi. You use slash commands 
+             and provide music, chat, and other features. Don't refer to yourself as an AI language model. When users 
+             call you with the 'chat' command, engage with them. For help, direct users to the 'help' command."""}]
+
+
+messages = initialize_messages()
 
 
 @boboAI.route("/generate", methods=["POST"])
 def generate():
+    global messages
     prompt = request.form["prompt"]
     messages.append({"role": "user", "content": prompt})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
-        max_tokens=500,
+        max_tokens=350,
     )
     output = response['choices'][0]['message']['content']
     messages.append({"role": "assistant", "content": output})
@@ -25,5 +34,6 @@ def generate():
 
 @boboAI.route("/reset", methods=["GET"])
 def reset():
-    messages.clear()
+    global messages
+    messages = initialize_messages()
     return "Chat reset."
