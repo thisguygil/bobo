142ea7c85e9a83dbd4a041397626df4096094e57
Gil Anavi <gil.j.anavi@gmail.com>
2023-06-20 14:07:16 +0300

    Added description to get method in Config

--- src/main/java/bobo/Config.java
+++ src/main/java/bobo/Config.java
@@ -3,11 +3,15 @@ package bobo;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-
     private static final Dotenv dotenv = Dotenv.load();
 
+    /**
+     * Get the value of the key from the .env file
+     *
+     * @param key the key to get the value of
+     * @return value of the key
+     */
     public static String get(String key) {
         return dotenv.get(key);
     }
-
 }
