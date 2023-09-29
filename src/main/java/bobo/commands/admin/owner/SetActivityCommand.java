f2356b95c3ab1ac75253297c0eeb6dae1402f92d
Gil Anavi <gil.j.anavi@gmail.com>
2023-09-29 12:29:05 -0500

    Added custom status to set-activity command

--- src/main/java/bobo/commands/admin/owner/SetActivityCommand.java
+++ src/main/java/bobo/commands/admin/owner/SetActivityCommand.java
@@ -3,12 +3,13 @@ package bobo.commands.admin.owner;
 import bobo.Bobo;
 import bobo.utils.SQLConnection;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 
 import java.sql.*;
-import java.util.Objects;
 
 public class SetActivityCommand extends AbstractOwner {
     private static final String createTableSQL = "CREATE TABLE IF NOT EXISTS activity (activity_type VARCHAR(255), activity_name VARCHAR(255) NOT NULL)";
@@ -20,18 +21,22 @@ public class SetActivityCommand extends AbstractOwner {
      * Creates a new set-activity command.
      */
     public SetActivityCommand() {
-        super(Commands.slash("set-activity", "Sets bobo's activity with specified type (playing, streaming, listening, watching, competing).")
-                .addSubcommands(
-                        new SubcommandData("playing", "Sets bobo's activity to playing.")
-                                .addOption(OptionType.STRING, "activity-name", "Activity to set.", true),
-                        new SubcommandData("streaming", "Sets bobo's activity to streaming.")
-                                .addOption(OptionType.STRING, "activity-name", "Activity to set.", true),
-                        new SubcommandData("listening", "Sets bobo's activity to listening.")
-                                .addOption(OptionType.STRING, "activity-name", "Activity to set.", true),
-                        new SubcommandData("watching", "Sets bobo's activity to watching.")
-                                .addOption(OptionType.STRING, "activity-name", "Activity to set.", true),
-                        new SubcommandData("competing", "Sets bobo's activity to competing.")
-                                .addOption(OptionType.STRING, "activity-name", "Activity to set.", true)
+        super(Commands.slash("set-activity", "Sets Bobo's activity.")
+                .addSubcommands(new SubcommandData("custom", "Sets Bobo's activity to a custom status.")
+                        .addOption(OptionType.STRING, "status", "Status to set.", true))
+                .addSubcommandGroups(new SubcommandGroupData("activity", "Sets Bobo's status to an activity.")
+                        .addSubcommands(
+                                new SubcommandData("playing", "Sets Bobo's activity to playing.")
+                                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                                new SubcommandData("streaming", "Sets Bobo's activity to streaming.")
+                                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                                new SubcommandData("listening", "Sets Bobo's activity to listening.")
+                                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                                new SubcommandData("watching", "Sets Bobo's activity to watching.")
+                                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                                new SubcommandData("competing", "Sets Bobo's activity to competing.")
+                                        .addOption(OptionType.STRING, "activity", "Activity to set.", true)
+                        )
                 )
         );
     }
@@ -41,7 +46,11 @@ public class SetActivityCommand extends AbstractOwner {
         event.deferReply().queue();
 
         String activityType = event.getSubcommandName();
-        String activityName = Objects.requireNonNull(event.getOption("activity-name")).getAsString();
+
+        assert activityType != null;
+        OptionMapping optionMapping = activityType.equals("custom") ? event.getOption("status") : event.getOption("activity");
+        assert optionMapping != null;
+        String activityName = optionMapping.getAsString();
 
         try (Connection connection = SQLConnection.getConnection()) {
             try (Statement statement = connection.createStatement()) {
@@ -61,11 +70,20 @@ public class SetActivityCommand extends AbstractOwner {
         }
 
         setActivity();
-        hook.editOriginal("Activity type set to **" + activityType + "**" + "\n" + "Activity set to **" + activityName + "**").queue();
+
+        String message = switch (activityType) {
+            case "playing" -> "Activity set to **Playing " + activityName + "**";
+            case "streaming" -> "Activity set to **Streaming " + activityName + "**";
+            case "listening" -> "Activity set to **Listening to " + activityName + "**";
+            case "watching" -> "Activity set to **Watching " + activityName + "**";
+            case "competing" -> "Activity set to **Competing in " + activityName + "**";
+            default -> "Status set to **" + activityName + "**";
+        };
+        hook.editOriginal(message).queue();
     }
 
     /**
-     * Sets bobo's activity
+     * Sets Bobo's activity.
      */
     public static void setActivity() {
         Activity activity = null;
@@ -77,10 +95,11 @@ public class SetActivityCommand extends AbstractOwner {
                 String activityName = resultSet.getString("activity_name");
                 activity = switch (activityType) {
                     case "playing" -> Activity.playing(activityName);
+                    case "streaming" -> Activity.streaming(activityName, "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
                     case "listening" -> Activity.listening(activityName);
                     case "watching" -> Activity.watching(activityName);
                     case "competing" -> Activity.competing(activityName);
-                    default -> Activity.streaming(activityName, "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                    default -> Activity.customStatus(activityName);
                 };
             } else {
                 return;
