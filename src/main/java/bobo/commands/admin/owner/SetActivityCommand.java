241bfaae9c84a979594dadb175d82dc61916d1ff
Gil Anavi <gil.j.anavi@gmail.com>
2023-10-02 19:15:07 -0500

    Added stream url option to /set-activity streaming

--- src/main/java/bobo/commands/admin/owner/SetActivityCommand.java
+++ src/main/java/bobo/commands/admin/owner/SetActivityCommand.java
@@ -9,11 +9,12 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 import java.sql.*;
+import java.util.Objects;
 
 public class SetActivityCommand extends AbstractOwner {
-    private static final String createTableSQL = "CREATE TABLE IF NOT EXISTS activity (activity_type VARCHAR(255), activity_name VARCHAR(255) NOT NULL)";
+    private static final String createTableSQL = "CREATE TABLE IF NOT EXISTS activity (activity_type VARCHAR(255), activity_name VARCHAR(255), stream_url VARCHAR(255))";
     private static final String deleteSQL = "DELETE FROM activity";
-    private static final String insertSQL = "INSERT INTO activity (activity_type, activity_name) VALUES (?, ?)";
+    private static final String insertSQL = "INSERT INTO activity (activity_type, activity_name, stream_url) VALUES (?, ?, ?)";
     private static final String selectSQL = "SELECT * FROM activity";
 
     /**
@@ -22,17 +23,18 @@ public class SetActivityCommand extends AbstractOwner {
     public SetActivityCommand() {
         super(Commands.slash("set-activity", "Sets Bobo's activity.")
                 .addSubcommands(new SubcommandData("custom", "Sets Bobo's activity to a custom status.")
-                        .addOption(OptionType.STRING, "status", "Status to set.", true),
-                new SubcommandData("playing", "Sets Bobo's activity to playing.")
-                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
-                new SubcommandData("streaming", "Sets Bobo's activity to streaming.")
-                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
-                new SubcommandData("listening", "Sets Bobo's activity to listening.")
-                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
-                new SubcommandData("watching", "Sets Bobo's activity to watching.")
-                        .addOption(OptionType.STRING, "activity", "Activity to set.", true),
-                new SubcommandData("competing", "Sets Bobo's activity to competing.")
-                        .addOption(OptionType.STRING, "activity", "Activity to set.", true))
+                                .addOption(OptionType.STRING, "status", "Status to set.", true),
+                        new SubcommandData("playing", "Sets Bobo's activity to playing.")
+                                .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                        new SubcommandData("streaming", "Sets Bobo's activity to streaming.")
+                                .addOption(OptionType.STRING, "activity", "Activity to set.", true)
+                                .addOption(OptionType.STRING, "url", "URL to stream.", true),
+                        new SubcommandData("listening", "Sets Bobo's activity to listening.")
+                                .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                        new SubcommandData("watching", "Sets Bobo's activity to watching.")
+                                .addOption(OptionType.STRING, "activity", "Activity to set.", true),
+                        new SubcommandData("competing", "Sets Bobo's activity to competing.")
+                                .addOption(OptionType.STRING, "activity", "Activity to set.", true))
         );
     }
 
@@ -41,11 +43,19 @@ public class SetActivityCommand extends AbstractOwner {
         event.deferReply().queue();
 
         String activityType = event.getSubcommandName();
-
         assert activityType != null;
-        OptionMapping optionMapping = activityType.equals("custom") ? event.getOption("status") : event.getOption("activity");
-        assert optionMapping != null;
-        String activityName = optionMapping.getAsString();
+        OptionMapping nameOption = activityType.equals("custom") ? event.getOption("status") : event.getOption("activity");
+        assert nameOption != null;
+        String activityName = nameOption.getAsString();
+
+        String streamURL = null;
+        if (activityType.equals("streaming")) {
+            streamURL = Objects.requireNonNull(event.getOption("url")).getAsString();
+            if (!Activity.isValidStreamingUrl(streamURL)) {
+                hook.editOriginal("Invalid stream URL: " + streamURL).queue();
+                return;
+            }
+        }
 
         try (Connection connection = SQLConnection.getConnection()) {
             try (Statement statement = connection.createStatement()) {
@@ -56,6 +66,7 @@ public class SetActivityCommand extends AbstractOwner {
             try (PreparedStatement insertStatement = connection.prepareStatement(insertSQL)) {
                 insertStatement.setString(1, activityType);
                 insertStatement.setString(2, activityName);
+                insertStatement.setString(3, streamURL);
                 insertStatement.executeUpdate();
             }
         } catch (SQLException e) {
@@ -68,7 +79,7 @@ public class SetActivityCommand extends AbstractOwner {
 
         String message = switch (activityType) {
             case "playing" -> "Activity set to **Playing " + activityName + "**";
-            case "streaming" -> "Activity set to **Streaming " + activityName + "**";
+            case "streaming" -> "Activity set to **Streaming [" + activityName + "](<" + streamURL + ">)**";
             case "listening" -> "Activity set to **Listening to " + activityName + "**";
             case "watching" -> "Activity set to **Watching " + activityName + "**";
             case "competing" -> "Activity set to **Competing in " + activityName + "**";
@@ -88,9 +99,10 @@ public class SetActivityCommand extends AbstractOwner {
             if (resultSet.next()) {
                 String activityType = resultSet.getString("activity_type");
                 String activityName = resultSet.getString("activity_name");
+                String streamURL = resultSet.getString("stream_url");
                 activity = switch (activityType) {
                     case "playing" -> Activity.playing(activityName);
-                    case "streaming" -> Activity.streaming(activityName, "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                    case "streaming" -> Activity.streaming(activityName, streamURL);
                     case "listening" -> Activity.listening(activityName);
                     case "watching" -> Activity.watching(activityName);
                     case "competing" -> Activity.competing(activityName);
