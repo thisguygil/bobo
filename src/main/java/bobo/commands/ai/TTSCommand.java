0865dd52488026c919ec5270ba081b87a2af33ec
Gil Anavi <gil.j.anavi@gmail.com>
2023-11-15 21:46:15 -0600

    Added tts voice change option

--- src/main/java/bobo/commands/ai/TTSCommand.java
+++ src/main/java/bobo/commands/ai/TTSCommand.java
@@ -2,7 +2,9 @@ package bobo.commands.ai;
 
 import bobo.commands.voice.JoinCommand;
 import bobo.lavaplayer.PlayerManager;
+import bobo.utils.SQLConnection;
 import com.theokanning.openai.audio.CreateSpeechRequest;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -10,6 +12,10 @@ import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Objects;
 
 /* NOTE: This also classifies as a music command (as it must use lavaplayer to play tts),
@@ -17,11 +23,13 @@ import java.util.Objects;
    since the AI is the bulk of the command's logic.
  */
 public class TTSCommand extends AbstractAI {
+    private static final String selectSQL = "SELECT voice FROM tts_voice WHERE guild_id = ?";
+
     /**
      * Creates a new TTS command.
      */
     public TTSCommand() {
-        super(Commands.slash("tts", "Sends a message to the AI to be said in the voice channel.")
+        super(Commands.slash("tts", "Generates an AI voice message for the voice channel.")
                 .addOption(OptionType.STRING, "message", "Message to be said.", true));
     }
 
@@ -34,7 +42,8 @@ public class TTSCommand extends AbstractAI {
     protected void handleAICommand() {
         event.deferReply().queue();
 
-        AudioManager audioManager = event.getGuildChannel().getGuild().getAudioManager();
+        Guild guild = event.getGuildChannel().getGuild();
+        AudioManager audioManager = guild.getAudioManager();
         if (!audioManager.isConnected()) {
             if (!JoinCommand.join(event)) {
                 return;
@@ -51,17 +60,34 @@ public class TTSCommand extends AbstractAI {
             }
         }
 
+        if (Objects.requireNonNull(guild.getSelfMember().getVoiceState()).isMuted()) {
+            hook.editOriginal("I can't be muted when using this command.").queue();
+            return;
+        }
+
         PlayerManager playerManager = PlayerManager.getInstance();
-        if (playerManager.getMusicManager(Objects.requireNonNull(event.getGuild())).player.getPlayingTrack() != null) {
+        if (playerManager.getMusicManager(guild).player.getPlayingTrack() != null) {
             event.getHook().editOriginal("Can't use TTS while a track is playing.").queue();
             return;
+        }
+
+        String voice = "onyx";
+        try (Connection connection = SQLConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(selectSQL)) {
+            statement.setString(1, guild.getId());
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                voice = resultSet.getString("voice");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         String message = Objects.requireNonNull(event.getOption("message")).getAsString();
         CreateSpeechRequest createSpeechRequest = CreateSpeechRequest.builder()
                 .model("tts-1")
                 .input(message)
-                .voice("onyx")
+                .voice(voice)
                 .build();
 
         File file;
@@ -76,7 +102,7 @@ public class TTSCommand extends AbstractAI {
         }
 
         playerManager.loadAndPlay(event, "tts.mp3", true);
-        hook.editOriginal("Playing TTS: **" + message + "**").queue(success -> {
+        hook.editOriginal("**Playing TTS:** " + message).queue(success -> {
             if (!file.delete()) {
                 System.err.println("Failed to delete file: " + file.getName());
             }
