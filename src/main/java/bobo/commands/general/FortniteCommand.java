01c09840beed9c20840dcfef5de24e582225dba7
Gil Anavi <gil.j.anavi@gmail.com>
2024-01-09 13:22:44 +0200

    Made Fortnite command asynchronous

--- src/main/java/bobo/commands/general/FortniteCommand.java
+++ src/main/java/bobo/commands/general/FortniteCommand.java
@@ -1,6 +1,7 @@
 package bobo.commands.general;
 
 import bobo.utils.FortniteAPI;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.utils.FileUpload;
@@ -11,6 +12,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class FortniteCommand extends AbstractGeneral {
     /**
@@ -33,33 +35,58 @@ public class FortniteCommand extends AbstractGeneral {
     @Override
     protected void handleGeneralCommand() {
         event.deferReply().queue();
-
+        var currentHook = hook;
         String subcommandName = Objects.requireNonNull(event.getSubcommandName());
+
         switch (subcommandName) {
-            case "shop" -> {
-                File file = convertBufferedImageToFile(FortniteAPI.getShopImage(), "shop");
-                if (file != null) {
-                    hook.editOriginalAttachments(FileUpload.fromData(file)).queue(success -> {
-                        if (!file.delete()) {
-                            System.err.println("Failed to delete file: " + file.getAbsolutePath());
-                        }
-                    });
-                } else {
-                    hook.editOriginal("Failed to get shop image.").queue();
+            case "shop" -> processShopCommand(currentHook);
+            case "map" -> processMapCommand(currentHook);
+        }
+    }
+
+    /**
+     * Processes the shop command.
+     * @param currentHook The current interaction hook.
+     */
+    private void processShopCommand(InteractionHook currentHook) {
+        CompletableFuture.supplyAsync(() -> convertBufferedImageToFile(FortniteAPI.getShopImage(), "shop"))
+                .thenAccept(file -> handleFileResponse(file, "Failed to get shop image.", currentHook))
+                .exceptionally(throwable -> {
+                    throwable.printStackTrace();
+                    currentHook.editOriginal("Error processing shop command.").queue();
+                    return null;
+                });
+    }
+
+    /**
+     * Processes the map command.
+     * @param currentHook The current interaction hook.
+     */
+    private void processMapCommand(InteractionHook currentHook) {
+        CompletableFuture.supplyAsync(() -> convertBufferedImageToFile(FortniteAPI.getMapImage(), "map"))
+                .thenAccept(file -> handleFileResponse(file, "Failed to get map image.", currentHook))
+                .exceptionally(throwable -> {
+                    throwable.printStackTrace();
+                    currentHook.editOriginal("Error processing map command.").queue();
+                    return null;
+                });
+    }
+
+    /**
+     * Handles the response for a file.
+     * @param file The file to send.
+     * @param errorMessage The error message to send if the file is null.
+     * @param currentHook The current interaction hook.
+     */
+    private void handleFileResponse(File file, String errorMessage, InteractionHook currentHook) {
+        if (file != null) {
+            currentHook.editOriginalAttachments(FileUpload.fromData(file)).queue(success -> {
+                if (!file.delete()) {
+                    System.err.println("Failed to delete file: " + file.getAbsolutePath());
                 }
-            }
-            case "map" -> {
-                File file = convertBufferedImageToFile(FortniteAPI.getMapImage(), "map");
-                if (file != null) {
-                    hook.editOriginalAttachments(FileUpload.fromData(file)).queue(success -> {
-                        if (!file.delete()) {
-                            System.err.println("Failed to delete file: " + file.getAbsolutePath());
-                        }
-                    });
-                } else {
-                    hook.editOriginal("Failed to get map image.").queue();
-                }
-            }
+            });
+        } else {
+            currentHook.editOriginal(errorMessage).queue();
         }
     }
 
