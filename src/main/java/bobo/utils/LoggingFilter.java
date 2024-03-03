9188038c593382bf13e8abab6db0e06b1fd2339d
Gil Anavi <gil.j.anavi@gmail.com>
2024-03-03 09:39:41 +0200

    Added logging filter for random audio exception

---
+++ src/main/java/bobo/utils/LoggingFilter.java
@@ -0,0 +1,16 @@
+package bobo.utils;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class LoggingFilter extends Filter<ILoggingEvent> {
+    @Override
+    public FilterReply decide(ILoggingEvent iLoggingEvent) {
+        // Filter out the random exception message
+        if (iLoggingEvent.getMessage().contains("There was some random exception while waiting for udp packets")) {
+            return FilterReply.DENY;
+        }
+        return FilterReply.NEUTRAL;
+    }
+}
