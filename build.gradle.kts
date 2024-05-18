5f208e78e364832b4c45531d297d8fcb8de96eb6
Gil Anavi <gil.j.anavi@gmail.com>
2024-05-17 22:05:17 -0500

    Update YouTube source to 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("com.github.ygimenez:Pagination-Utils:4.0.6")
     implementation("com.theokanning.openai-gpt3-java:service:0.18.2")
     implementation("dev.arbjerg:lavaplayer:2.1.2")
-    implementation("dev.lavalink.youtube:v2:1.2.0")
+    implementation("dev.lavalink.youtube:v2:1.3.0")
     implementation("com.github.topi314.lavasrc:lavasrc:4.1.1")
     implementation("com.github.topi314.lavalyrics:lavalyrics:1.0.0")
     implementation("com.google.api-client:google-api-client:2.3.0")
