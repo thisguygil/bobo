a3e67b34a62ca0e54e1681b116e4d2cd52b0099f
Gil Anavi <gil.j.anavi@gmail.com>
2024-05-17 22:04:57 -0500

    Update LavaSrc to 4.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("com.theokanning.openai-gpt3-java:service:0.18.2")
     implementation("dev.arbjerg:lavaplayer:2.1.2")
     implementation("dev.lavalink.youtube:v2:1.2.0")
-    implementation("com.github.topi314.lavasrc:lavasrc:4.1.0")
+    implementation("com.github.topi314.lavasrc:lavasrc:4.1.1")
     implementation("com.github.topi314.lavalyrics:lavalyrics:1.0.0")
     implementation("com.google.api-client:google-api-client:2.3.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.35.0")
