52cae3a7a6da095a0c247cf4bce5d2effedb43be
thisguygil <gil.j.anavi@gmail.com>
2024-12-27 11:50:57 -0600

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
     // LavaPlayer
     implementation("dev.arbjerg:lavaplayer:2.2.2")
-    implementation("dev.lavalink.youtube:v2:1.11.1")
+    implementation("dev.lavalink.youtube:v2:1.11.3")
     implementation("com.github.topi314.lavasrc:lavasrc:4.2.0")
     implementation("com.github.topi314.lavalyrics:lavalyrics:1.0.0")
 
@@ -34,10 +34,10 @@ dependencies {
     implementation("com.github.ygimenez:Pagination-Utils:4.1.0")
 
     // Google APIs
-    implementation("com.google.api-client:google-api-client:2.7.0")
+    implementation("com.google.api-client:google-api-client:2.7.1")
     implementation("com.google.apis:google-api-services-customsearch:v1-rev20240821-2.0.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.36.0")
-    implementation("com.google.apis:google-api-services-youtube:v3-rev20241105-2.0.0")
+    implementation("com.google.apis:google-api-services-youtube:v3-rev20241203-2.0.0")
     implementation("com.google.guava:guava:33.3.1-jre")
 
     // Image Processing
@@ -54,12 +54,12 @@ dependencies {
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
 
     // Other API Wrappers
-    implementation("se.michaelthelin.spotify:spotify-web-api-java:9.0.0")
-    implementation("io.github.sashirestela:simple-openai:3.10.0")
+    implementation("se.michaelthelin.spotify:spotify-web-api-java:9.1.0")
+    implementation("io.github.sashirestela:simple-openai:3.12.0")
 
     // Other Java Utilities
     implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
-    implementation("org.apache.commons:commons-text:1.12.0")
+    implementation("org.apache.commons:commons-text:1.13.0")
     implementation("org.apache.commons:commons-lang3:3.17.0")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
     implementation("commons-validator:commons-validator:1.9.0")
