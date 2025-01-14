606b69c129a99f08454880bc266a29319bc7fe0f
thisguygil <gil.j.anavi@gmail.com>
2025-01-13 21:56:36 -0600

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,7 @@ java {
 
 repositories {
     mavenCentral() // Most of the dependencies are in Maven Central
-    maven(url = "https://jitpack.io") // For Spotify Web API
-    maven(url = "https://maven.lavalink.dev/releases") // For LavaPlayer and LavaLink YouTube
+    maven(url = "https://maven.lavalink.dev/releases") // For LavaLink YouTube source
     maven(url = "https://maven.topi.wtf/releases") // For LavaSrc and LavaLyrics
 }
 
@@ -27,7 +26,7 @@ dependencies {
     // LavaPlayer
     implementation("dev.arbjerg:lavaplayer:2.2.2")
     implementation("dev.lavalink.youtube:v2:1.11.3")
-    implementation("com.github.topi314.lavasrc:lavasrc:4.2.0")
+    implementation("com.github.topi314.lavasrc:lavasrc:4.3.0")
     implementation("com.github.topi314.lavalyrics:lavalyrics:1.0.0")
 
     // JDA Utilities
@@ -36,9 +35,7 @@ dependencies {
     // Google APIs
     implementation("com.google.api-client:google-api-client:2.7.1")
     implementation("com.google.apis:google-api-services-customsearch:v1-rev20240821-2.0.0")
-    implementation("com.google.oauth-client:google-oauth-client-jetty:1.36.0")
-    implementation("com.google.apis:google-api-services-youtube:v3-rev20241203-2.0.0")
-    implementation("com.google.guava:guava:33.3.1-jre")
+    implementation("com.google.apis:google-api-services-youtube:v3-rev20250101-2.0.0")
 
     // Image Processing
     implementation("net.coobird:thumbnailator:0.4.20")
@@ -48,22 +45,21 @@ dependencies {
     implementation("com.mysql:mysql-connector-j:9.1.0")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:1.5.15")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
 
     // Local Testing
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.0")
 
     // Other API Wrappers
-    implementation("se.michaelthelin.spotify:spotify-web-api-java:9.1.0")
-    implementation("com.openai:openai-java:0.9.0")
+    implementation("se.michaelthelin.spotify:spotify-web-api-java:9.1.1")
+    implementation("com.openai:openai-java:0.11.2")
 
     // Other Java Utilities
-    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
     implementation("org.reflections:reflections:0.10.2")
     implementation("org.apache.commons:commons-text:1.13.0")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
     implementation("commons-validator:commons-validator:1.9.0")
-    implementation("org.json:json:20240303")
+    implementation("org.json:json:20250107")
 }
 
 tasks.jar {
