639fe2b6447b426486ec92ac7a357156b36a4f31
thisguygil <gil.j.anavi@gmail.com>
2025-03-09 21:16:23 -0500

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,12 @@ repositories {
 dependencies {
     // Discord API and Utilities
     implementation("net.dv8tion:JDA:5.3.0")
-    implementation("com.github.ygimenez:Pagination-Utils:4.1.0")
+    implementation("com.github.ygimenez:Pagination-Utils:4.1.1")
 
     // LavaPlayer
     implementation("dev.arbjerg:lavaplayer:2.2.3")
-    implementation("dev.lavalink.youtube:v2:1.11.4")
-    implementation("com.github.topi314.lavasrc:lavasrc:4.4.1")
+    implementation("dev.lavalink.youtube:v2:1.11.5")
+    implementation("com.github.topi314.lavasrc:lavasrc:4.4.2")
     implementation("com.github.topi314.lavalyrics:lavalyrics:1.0.0")
 
     // Google APIs
@@ -48,11 +48,11 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.16")
 
     // Local Testing
-    implementation("io.github.cdimascio:dotenv-kotlin:6.5.0")
+    implementation("io.github.cdimascio:dotenv-java:3.2.0")
 
     // Other API Wrappers
     implementation("se.michaelthelin.spotify:spotify-web-api-java:9.1.1")
-    implementation("com.openai:openai-java:0.22.0")
+    implementation("com.openai:openai-java:0.33.0")
 
     // Other Java Utilities
     implementation("org.reflections:reflections:0.10.2")
